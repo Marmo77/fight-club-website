e16a35a54c25342f7684a2f526e37b8ccf8fc576
Marmo77 <matyskamela@gmail.com>
2025-12-24 15:04:20 +0100

    ai cards

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
 import Test from "../Test";
+import Trainers from "./Trainers";
 
 const Home = () => {
   return (
     <section className="mx-auto flex flex-col justify-center items-center">
       <Hero />
+      <Trainers />
       <Test />
     </section>
   );
